d5acb5cd81f5a078b5d561444f62d2043bb8f116
Marta Manchur <mmmz1004@gmail.com>
2019-05-15 14:46:11 +0300

    made header hide on scroll

--- Ujin/Ujin/ClientApp/src/app/desktop/header/header.component.ts
+++ Ujin/Ujin/ClientApp/src/app/desktop/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+import {
+  Component, OnInit, OnDestroy, AfterViewInit,
+  trigger, state, style, transition, animate, HostBinding
+} from '@angular/core';
 import { LocaleService } from 'angular-l10n';
 import { languages } from '../../configs/localization.config';
+import { fromEvent } from 'rxjs/observable/fromEvent';
+import { throttleTime, map, distinctUntilChanged, pairwise, share, filter } from 'rxjs/operators';
 import { HeaderGaService } from '../../googleAnalytics/header-ga.service';
 import { SocialService } from '../../services/social.service';
 
 @Component({
   selector: 'app-desktop-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.less']
+  styleUrls: ['./header.component.less'],
+  animations: [
+    trigger('toggle', [
+      state(
+        "hidden",
+        style({ transform: 'translateY(-100%)' })
+      ),
+      state(
+        "visible",
+        style({ transform: 'translateY(0)' })
+      ),
+      transition('* => *', animate('200ms ease-in'))
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit, OnDestroy, AfterViewInit {
+
+  private _headerVisibility: VisibilityState = VisibilityState.Visible;
 
   public languages: { name: string, code: string }[];
 
@@ -35,10 +55,35 @@ export class HeaderComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngAfterViewInit() {
     this.gaService.registerEvents();
+
+    const scroll = fromEvent(window, 'scroll').pipe(
+      throttleTime(10),
+      map(() => window.pageYOffset),
+      pairwise(),
+      map(([y1, y2]): Direction => (y2 < y1 ? Direction.Up : Direction.Down)),
+      distinctUntilChanged(),
+      share()
+    );
+
+    const scrollUp = scroll.pipe(
+      filter(direction => direction === Direction.Up)
+    );
+
+    const scrollDown = scroll.pipe(
+      filter(direction => direction === Direction.Down)
+    );
+
+    scrollUp.subscribe(() => this._headerVisibility = VisibilityState.Visible);
+    scrollDown.subscribe(() => this._headerVisibility = VisibilityState.Hidden);
   }
 
   ngOnDestroy() {
     this.gaService.dispose();
+  }
+
+  @HostBinding('@toggle')
+  public get headerVisibility(): VisibilityState {
+    return this._headerVisibility;
   }
 
   public isLangSelected(lang: { name: string, code: string }): boolean {
@@ -52,4 +97,14 @@ export class HeaderComponent implements OnInit, OnDestroy, AfterViewInit {
     this._locale.setCurrentLanguage(lang.code);
   }
 
+}
+
+enum VisibilityState {
+  Visible = 'visible',
+  Hidden = 'hidden'
+}
+
+enum Direction {
+  Up = 'Up',
+  Down = 'Down'
 }
--- Ujin/Ujin/ClientApp/src/app/mobile/header/header.component.ts
+++ Ujin/Ujin/ClientApp/src/app/mobile/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit, HostListener, OnDestroy, AfterViewInit } from '@angular/core';
+import {
+  Component, OnInit, HostListener, OnDestroy, AfterViewInit,
+  trigger, state, style, transition, animate, HostBinding
+} from '@angular/core';
+import { fromEvent } from 'rxjs/observable/fromEvent';
+import { throttleTime, map, distinctUntilChanged, pairwise, share, filter } from 'rxjs/operators';
 import { HeaderGaService } from '../../googleAnalytics/header-ga.service';
 import { SocialService } from '../../services/social.service';
 
 @Component({
   selector: 'app-mobile-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.less']
+  styleUrls: ['./header.component.less'],
+  animations: [
+    trigger('toggle', [
+      state(
+        "hidden",
+        style({ transform: 'translateY(-100%)' })
+      ),
+      state(
+        "visible",
+        style({ transform: 'translateY(0)' })
+      ),
+      transition('* => *', animate('200ms ease-in'))
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit, OnDestroy, AfterViewInit {
 
   private _menuState: MenuState = MenuState.Closed;
+
+  private _headerVisibility: VisibilityState = VisibilityState.Visible;
 
   public buttonsVisible: boolean = false;
 
@@ -27,10 +47,38 @@ export class HeaderComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngAfterViewInit() {
     this.gaService.registerEvents();
+
+    const scroll = fromEvent(window, 'scroll').pipe(
+      throttleTime(10),
+      map(() => window.pageYOffset),
+      pairwise(),
+      map(([y1, y2]): Direction => (y2 < y1 ? Direction.Up : Direction.Down)),
+      distinctUntilChanged(),
+      share()
+    );
+
+    const scrollUp = scroll.pipe(
+      filter(direction => direction === Direction.Up)
+    );
+
+    const scrollDown = scroll.pipe(
+      filter(direction => direction === Direction.Down)
+    );
+
+    scrollUp.subscribe(() => this._headerVisibility = VisibilityState.Visible);
+    scrollDown.subscribe(() => {
+      this._headerVisibility = VisibilityState.Hidden;
+      this.clickout();
+    });
   }
 
   ngOnDestroy() {
     this.gaService.dispose();
+  }
+
+  @HostBinding('@toggle')
+  public get headerVisibility(): VisibilityState {
+    return this._headerVisibility;
   }
 
   public get menuOpened(): boolean {
@@ -65,4 +113,14 @@ export class HeaderComponent implements OnInit, OnDestroy, AfterViewInit {
 enum MenuState {
   Opened = "opened",
   Closed = "closed"
+}
+
+enum VisibilityState {
+  Visible = 'visible',
+  Hidden = 'hidden'
+}
+
+enum Direction {
+  Up = 'Up',
+  Down = 'Down'
 }

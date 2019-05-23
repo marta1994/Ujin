d253c98632eb8f44ffad35d21d6a5f075857d689
Marta Manchur <mmmz1004@gmail.com>
2019-05-23 09:41:02 +0300

    added EF core

---
+++ Storage/UjinContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Storage.Models;
+
+namespace Storage
+{
+    public class UjinContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}

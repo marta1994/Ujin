fbb27f458b44fc0086e5b134f326b2c1aae44528
Marta Manchur <mmmz1004@gmail.com>
2019-08-01 20:36:21 +0300

    fixed Ios clipboard copy

--- Ujin.Web/ClientApp/src/app/services/clipboard.service.ts
+++ Ujin.Web/ClientApp/src/app/services/clipboard.service.ts
@@ -14,9 +14,20 @@ export class ClipboardService {
     selBox.style.top = '0';
     selBox.style.opacity = '0';
     selBox.value = text;
+    selBox.setAttribute("contenteditable", "true");
+    selBox.removeAttribute("readonly");
     document.body.appendChild(selBox);
+
     selBox.focus();
     selBox.select();
+
+    let range = document.createRange();
+    range.selectNodeContents(selBox);
+    var s = window.getSelection();
+    s.removeAllRanges();
+    s.addRange(range);
+    selBox.setSelectionRange(0, 999999);
+
     document.execCommand('copy');
     document.body.removeChild(selBox);
   }

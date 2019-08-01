1d5e6c07a02d97db1a76a363965ae53a955aabd6
Marta Manchur <mmmz1004@gmail.com>
2019-08-01 21:36:52 +0300

    widget: change config value on swipe

--- Ujin.Web/ClientApp/src/app/model/widget/selector-config/selector-config.component.ts
+++ Ujin.Web/ClientApp/src/app/model/widget/selector-config/selector-config.component.ts
@@ -3,94 +3,121 @@ import { SelectorConfiguration, OptionsSource, Gemstone, NamedEntity, CustomOpti
 import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
-    selector: 'app-selector-config',
-    templateUrl: './selector-config.component.html',
-    styleUrls: ['./selector-config.component.less'],
-    animations: [
-        trigger('collapseSubMenu', [
-            state('void', style({
-                height: 0
-            })),
-            transition('* -> void', animate('0.3s ease')),
-        ])
-    ]
+  selector: 'app-selector-config',
+  templateUrl: './selector-config.component.html',
+  styleUrls: ['./selector-config.component.less'],
+  animations: [
+    trigger('collapseSubMenu', [
+      state('void', style({
+        height: 0
+      })),
+      transition('* -> void', animate('0.3s ease')),
+    ])
+  ]
 })
 export class SelectorConfigComponent implements OnInit, OnChanges {
 
-    @Input()
-    public configuration: SelectorConfiguration;
+  @Input()
+  public configuration: SelectorConfiguration;
 
-    @Output()
-    public onChange: EventEmitter<string> = new EventEmitter<string>();
+  @Output()
+  public onChange: EventEmitter<string> = new EventEmitter<string>();
 
-    public OptionsSource = OptionsSource;
+  public OptionsSource = OptionsSource;
 
-    public gemstones: GemNode[];
+  public gemstones: GemNode[];
 
-    public selectedNode: GemNode;
+  public selectedNode: GemNode;
 
-    public selectedGem: Gemstone;
+  public selectedGem: Gemstone;
 
-    constructor() { }
+  constructor() { }
 
-    ngOnInit() {
+  ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (!changes.configuration || !this.configuration) return;
+    if (this.configuration.optionsSource !== OptionsSource.Gemstone) return;
+    this.gemstones = [];
+    this.configuration.gemstoneSource.forEach(g => {
+      let gemNode = this.gemstones.find(gem => gem.classId === g.gemstoneClass.identifier);
+      if (gemNode == null) {
+        gemNode = new GemNode();
+        gemNode.classId = g.gemstoneClass.identifier;
+        gemNode.nameKey = g.gemstoneClass.nameKey;
+        gemNode.color = g.color.colorHexCode;
+        gemNode.children = [];
+        this.gemstones.push(gemNode);
+      }
+      gemNode.children.push(g);
+    });
+    this.gemstones.forEach(g => g.children = g.children.sort(
+      (c1, c2) => c1.gemstoneSource.identifier.localeCompare(c2.gemstoneSource.identifier)));
+    this.selectedGem = this.configuration.gemstoneSource.find(
+      g => g.identifier.toLowerCase() === (this.configuration.value + "").toLowerCase());
+    this.selectedNode = this.gemstones.find(gn => gn.classId.toLowerCase() === this.selectedGem.gemstoneClass.identifier.toLowerCase());
+  }
+
+  public shiftValue(shift: number) {
+    let shiftInd = (ind, mod) => {
+      return (ind + shift + mod) % mod;
     }
 
-    ngOnChanges(changes: SimpleChanges) {
-        if (!changes.configuration || !this.configuration) return;
-        if (this.configuration.optionsSource !== OptionsSource.Gemstone) return;
-        this.gemstones = [];
-        this.configuration.gemstoneSource.forEach(g => {
-            let gemNode = this.gemstones.find(gem => gem.classId === g.gemstoneClass.identifier);
-            if (gemNode == null) {
-                gemNode = new GemNode();
-                gemNode.classId = g.gemstoneClass.identifier;
-                gemNode.nameKey = g.gemstoneClass.nameKey;
-                gemNode.color = g.color.colorHexCode;
-                gemNode.children = [];
-                this.gemstones.push(gemNode);
-            }
-            gemNode.children.push(g);
-        });
-        this.gemstones.forEach(g => g.children = g.children.sort(
-            (c1, c2) => c1.gemstoneSource.identifier.localeCompare(c2.gemstoneSource.identifier)));
-        this.selectedGem = this.configuration.gemstoneSource.find(
-            g => g.identifier.toLowerCase() === (this.configuration.value + "").toLowerCase());
-        this.selectedNode = this.gemstones.find(gn => gn.classId.toLowerCase() === this.selectedGem.gemstoneClass.identifier.toLowerCase());
+    switch (this.configuration.optionsSource) {
+      case OptionsSource.Gemstone: {
+        let gemInd = this.gemstones.indexOf(this.selectedNode);
+        gemInd = shiftInd(gemInd, this.gemstones.length);
+        this.selectGemClass(this.gemstones[gemInd]);
+        break;
+      }
+      case OptionsSource.Metal: {
+        let ind = this.configuration.metalSource.findIndex(s => this.isSelected(s));
+        ind = shiftInd(ind, this.configuration.metalSource.length);
+        this.selectItem(this.configuration.metalSource[ind].identifier);
+        break;
+      }
+      case OptionsSource.Custom: {
+        let ind = this.configuration.customOptions.findIndex(s => this.isSelected(s));
+        ind = shiftInd(ind, this.configuration.customOptions.length);
+        this.selectItem(this.configuration.customOptions[ind].identifier);
+        break;
+      }
     }
+  }
 
-    public isSelected(item: GemNode | Gemstone | NamedEntity | CustomOption): boolean {
-        if (item instanceof GemNode) {
-            return item == this.selectedNode;
-        }
-        if (item instanceof Gemstone) {
-            return item == this.selectedGem;
-        }
-        return (this.configuration.value + "").toLowerCase() === item.identifier.toLowerCase();
+  public isSelected(item: GemNode | Gemstone | NamedEntity | CustomOption): boolean {
+    if (item instanceof GemNode) {
+      return item == this.selectedNode;
     }
+    if (item instanceof Gemstone) {
+      return item == this.selectedGem;
+    }
+    return (this.configuration.value + "").toLowerCase() === item.identifier.toLowerCase();
+  }
 
-    public selectItem(identifier: string) {
-        if (this.configuration.value === identifier) return;
-        this.configuration.value = identifier;
-        this.onChange.emit(this.configuration.identifier);
-    }
+  public selectItem(identifier: string) {
+    if (this.configuration.value === identifier) return;
+    this.configuration.value = identifier;
+    this.onChange.emit(this.configuration.identifier);
+  }
 
-    public selectGemClass(gemNode: GemNode) {
-        if (this.selectedNode === gemNode) return;
-        this.selectedNode = gemNode;
-        var gem = this.selectedNode.children.find(
-            g => g.gemstoneSource.identifier.toLowerCase() === this.selectedGem.gemstoneSource.identifier.toLowerCase());
-        if (gem == null)
-            gem = this.selectedNode.children[0];
-        this.selectGem(gem);
-    }
+  public selectGemClass(gemNode: GemNode) {
+    if (this.selectedNode === gemNode) return;
+    this.selectedNode = gemNode;
+    var gem = this.selectedNode.children.find(
+      g => g.gemstoneSource.identifier.toLowerCase() === this.selectedGem.gemstoneSource.identifier.toLowerCase());
+    if (gem == null)
+      gem = this.selectedNode.children[0];
+    this.selectGem(gem);
+  }
 
-    public selectGem(gem: Gemstone) {
-        if (this.selectedGem === gem) return;
-        this.selectedGem = gem;
-        this.configuration.value = this.selectedGem.identifier;
-        this.onChange.emit(this.configuration.identifier);
-    }
+  public selectGem(gem: Gemstone) {
+    if (this.selectedGem === gem) return;
+    this.selectedGem = gem;
+    this.configuration.value = this.selectedGem.identifier;
+    this.onChange.emit(this.configuration.identifier);
+  }
 }
 
 class GemNode {
--- Ujin.Web/ClientApp/src/app/model/widget/widget.component.ts
+++ Ujin.Web/ClientApp/src/app/model/widget/widget.component.ts
@@ -1,122 +1,134 @@
-import { Component, OnInit, OnChanges, Input, SimpleChanges, EventEmitter, Output, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChanges, EventEmitter, Output, ChangeDetectorRef, ViewChildren, QueryList } from '@angular/core';
 import { JewelryModel, Configuration, JewelryModelConfigType } from '../models';
 import { trigger, transition, useAnimation, state, style, animate } from '@angular/animations';
 import { fadeOut, fadeIn } from 'ng-animate';
+import { SelectorConfigComponent } from './selector-config/selector-config.component';
 
 @Component({
-    selector: 'app-widget',
-    templateUrl: './widget.component.html',
-    styleUrls: ['./widget.component.less'],
-    animations: [
-        trigger('widgetImg', [
-            transition('* => out',
-                useAnimation(fadeOut, { params: { timing: 0.1 } })),
-            transition('* => in',
-                useAnimation(fadeIn, { params: { timing: 0.2 } })),
-            state('loading', style({
-                opacity: 0
-            }))])
-    ]
+  selector: 'app-widget',
+  templateUrl: './widget.component.html',
+  styleUrls: ['./widget.component.less'],
+  animations: [
+    trigger('widgetImg', [
+      transition('* => out',
+        useAnimation(fadeOut, { params: { timing: 0.1 } })),
+      transition('* => in',
+        useAnimation(fadeIn, { params: { timing: 0.2 } })),
+      state('loading', style({
+        opacity: 0
+      }))])
+  ]
 })
 export class WidgetComponent implements OnInit, OnChanges {
 
-    @Input()
-    public model: JewelryModel;
+  @Input()
+  public model: JewelryModel;
 
-    @Input()
-    public currentImages: string[] = [];
+  @Input()
+  public currentImages: string[] = [];
 
-    @Output()
-    public onChange: EventEmitter<string> = new EventEmitter<string>();
+  @Output()
+  public onChange: EventEmitter<string> = new EventEmitter<string>();
 
-    public selectedConfig: Configuration;
+  @ViewChildren(SelectorConfigComponent)
+  public selectorConfigs: QueryList<SelectorConfigComponent>;
 
-    public JewelryModelConfigType = JewelryModelConfigType;
+  public selectedConfig: Configuration;
 
-    public imageIndexes: number[];
+  public JewelryModelConfigType = JewelryModelConfigType;
 
-    constructor(private _changeDetector: ChangeDetectorRef) { }
+  public imageIndexes: number[];
 
-    ngOnInit() {
+  constructor(private _changeDetector: ChangeDetectorRef) { }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.currentImages) {
+      var oldImages = changes.currentImages.previousValue;
+      var currImages = changes.currentImages.currentValue;
+      if (!this.areStrArraysEqual(oldImages, currImages))
+        this.triggerAnimation();
     }
+    if (changes.model && this.model) {
+      this.selectedConfig = this.model.configurations[0];
+      this.imageIndexes = [];
+      for (let i = 0; i < this.model.imagesCount; ++i)
+        this.imageIndexes.push(i);
+    }
+  }
 
-    ngOnChanges(changes: SimpleChanges) {
-        if (changes.currentImages) {
-            var oldImages = changes.currentImages.previousValue;
-            var currImages = changes.currentImages.currentValue;
-            if (!this.areStrArraysEqual(oldImages, currImages))
-                this.triggerAnimation();
+  public swipe(shift: number) {
+    this.selectorConfigs.forEach(sc => {
+      if (sc.configuration === this.selectedConfig) {
+        sc.shiftValue(shift);
+      }
+    });
+  }
+
+  public selectConfig(config: Configuration) {
+    this.selectedConfig = config;
+  }
+
+  public isSelected(config: Configuration): boolean {
+    return this.selectedConfig === config;
+  }
+
+  public modelChanged(configId: string) {
+    this.currentConfigId = configId;
+    this.onChange.emit(this.currentConfigId);
+  }
+
+  private areStrArraysEqual(ar1: string[], ar2: string[]) {
+    if (ar1 == null && ar2 == null) return true;
+    if (ar1 == null || ar2 == null) return false;
+    if (ar1.length != ar2.length) return false;
+    return ar1.find(el1 => ar2.find(el2 => el2 === el1) == null) == null;
+  }
+
+  private triggerAnimation() {
+    this.imgAnimate = ImgAnimateState.Out;
+    this.loadedIndexes = [];
+    this.currentImages.forEach((img, ind) => {
+      let image = new Image();
+      image.onload = () => this.onImageLoded(ind);
+      image.src = img;
+    });
+  }
+
+  private currentConfigId: string;
+  public imgAnimate: ImgAnimateState;
+  private loadedIndexes: number[] = [];
+
+  public imgAnimateDone(event) {
+    switch (event.toState) {
+      case ImgAnimateState.Out:
+        if (this.currentConfigId != null) {
+          this.currentConfigId = null;
         }
-        if (changes.model && this.model) {
-            this.selectedConfig = this.model.configurations[0];
-            this.imageIndexes = [];
-            for (let i = 0; i < this.model.imagesCount; ++i)
-                this.imageIndexes.push(i);
-        }
-    }
-
-    public selectConfig(config: Configuration) {
-        this.selectedConfig = config;
-    }
-
-    public isSelected(config: Configuration): boolean {
-        return this.selectedConfig === config;
-    }
-
-    public modelChanged(configId: string) {
-        this.currentConfigId = configId;
-        this.onChange.emit(this.currentConfigId);
-    }
-
-    private areStrArraysEqual(ar1: string[], ar2: string[]) {
-        if (ar1 == null && ar2 == null) return true;
-        if (ar1 == null || ar2 == null) return false;
-        if (ar1.length != ar2.length) return false;
-        return ar1.find(el1 => ar2.find(el2 => el2 === el1) == null) == null;
-    }
-
-    private triggerAnimation() {
-        this.imgAnimate = ImgAnimateState.Out;
-        this.loadedIndexes = [];
-        this.currentImages.forEach((img, ind) => {
-            let image = new Image();
-            image.onload = () => this.onImageLoded(ind);
-            image.src = img;
-        });
-    }
-    
-    private currentConfigId: string;
-    public imgAnimate: ImgAnimateState;
-    private loadedIndexes: number[] = [];
-
-    public imgAnimateDone(event) {
-        switch (event.toState) {
-            case ImgAnimateState.Out:
-                if (this.currentConfigId != null) {
-                    this.currentConfigId = null;
-                }
-                this.imgAnimate = ImgAnimateState.Loading;
-                this.tryStartInAnimation();
-                break;
-            case ImgAnimateState.In:
-                this.imgAnimate = ImgAnimateState.None;
-                this._changeDetector.detectChanges();
-                break;
-        }
-    }
-
-    public onImageLoded(index: number) {
-        if (this.loadedIndexes.indexOf(index) < 0)
-            this.loadedIndexes.push(index);
-        if (this.imgAnimate != ImgAnimateState.Loading) return;
+        this.imgAnimate = ImgAnimateState.Loading;
         this.tryStartInAnimation();
+        break;
+      case ImgAnimateState.In:
+        this.imgAnimate = ImgAnimateState.None;
+        this._changeDetector.detectChanges();
+        break;
     }
+  }
 
-    private tryStartInAnimation() {
-        let loaded = this.loadedIndexes.length === this.currentImages.length;
-        if (!loaded) return;
-        this.imgAnimate = ImgAnimateState.In;
-    }
+  public onImageLoded(index: number) {
+    if (this.loadedIndexes.indexOf(index) < 0)
+      this.loadedIndexes.push(index);
+    if (this.imgAnimate != ImgAnimateState.Loading) return;
+    this.tryStartInAnimation();
+  }
+
+  private tryStartInAnimation() {
+    let loaded = this.loadedIndexes.length === this.currentImages.length;
+    if (!loaded) return;
+    this.imgAnimate = ImgAnimateState.In;
+  }
 }
 
 enum ImgAnimateState {
